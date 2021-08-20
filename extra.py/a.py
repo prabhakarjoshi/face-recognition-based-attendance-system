e6aa342b3c5f4ae992c4737c3f16356098e09861
prabhakarjoshi <70520718+prabhakarjoshi@users.noreply.github.com>
2021-08-20 23:59:41 +0530

    Add files via upload

---
+++ extra.py/a.py
@@ -0,0 +1,22 @@
+# from tkinter import *
+# # from tkinter import font 
+# import tkinter.messagebox
+# from PIL import Image,ImageTk
+
+# def abc():
+#     print(name.get())
+
+
+# root1=Tk()
+# root1.title("Login or signup")
+# name=StringVar()
+# mobile=StringVar()
+# passw=StringVar()
+# label_1 = Label(root1, text="Name :",width=20, font=("bold", 10))
+# label_1.grid(row=0,column=0,padx=40,pady=40)
+# entry_1 = Entry(root1,textvar=name)
+# entry_1.grid(row=0,column=1,padx=40,pady=40)
+
+# but_login=Button(root1, text='Log In',width=12,bg='brown',fg='white',command=abc).grid(row=4,column=1,columnspan=3,padx=40,pady=40)
+# # root.bind("<Return>",database)
+# root1.mainloop()
