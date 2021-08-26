47b0b406732ee77a81cfa4fce06c0681142a3e89
prabhakarjoshi <70520718+prabhakarjoshi@users.noreply.github.com>
2021-08-26 23:08:17 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,31 @@
+# from tkinter import *
+# # from tkinter import font 
+# import tkinter.messagebox
+# from PIL import Image,ImageTk
+
+# def abc():
+#     print(name.get())
+
+
+# root1=Tk()
+# root1.title("Login or signup")
+# name=StringVar()
+# mobile=StringVar()
+# passw=StringVar()
+# label_1 = Label(root1, text="Name :",width=20, font=("bold", 10))
+# label_1.grid(row=0,column=0,padx=40,pady=40)
+# entry_1 = Entry(root1,textvar=name)
+# entry_1.grid(row=0,column=1,padx=40,pady=40)
+
+# but_login=Button(root1, text='Log In',width=12,bg='brown',fg='white',command=abc).grid(row=4,column=1,columnspan=3,padx=40,pady=40)
+# # root.bind("<Return>",database)
+# root1.mainloop()
+from GUI.Dashboard_UI import Dashboard_UI_Fun
+# from Add_Student_UI import Add_Student_UI_Fun
+
+
+a=input()
+print(a)
+if a==a:
+    print("L")
+    Dashboard_UI_Fun()
--- main1.py
+++ main1.py
@@ -1,3 +1,7 @@
 from GUI.Landing_Page_GUI import Landing_Page_GUI_Fun
-
 Landing_Page_GUI_Fun()
+
+
+
+
+# python -m pip install --user opencv-contrib-python
