fd62210fb70fdef95e4e19f71fe08f008a860c29
prabhakarjoshi <70520718+prabhakarjoshi@users.noreply.github.com>
2021-08-26 23:06:58 +0530

    Add files via upload

--- DB/Login_Validate_db.py
+++ DB/Login_Validate_db.py
@@ -1,3 +1,4 @@
+from os import error
 from tkinter import *
 import tkinter.messagebox
 import mysql.connector
@@ -5,37 +6,37 @@ from GUI.Dashboard_UI import Dashboard_UI_Fun
 
 
 def Login_Validate_fun(login,name,code,passw,root1):
-    
-    mydb=mysql.connector.connect(host="localhost",
-    user="root",
-    password="",
-    database="face-recognition")
-    cursor = mydb.cursor()
-    if not mydb.is_connected:
-        tkinter.messagebox.showinfo("Error while connecting to the database!")
-    else:
-        if login:
-            query1 = "SELECT `password` FROM `login` WHERE username='"+name+"'"
-            cursor.execute(query1)
-
-            for row in cursor: 
-                if row[0]==passw:
-                    print("loginn successfull")
-                    break   
-
-            cursor.close()
-            mydb.close()
-            
-            
-
-            
+    try:
+        mydb=mysql.connector.connect(host="localhost",
+        user="root",
+        password="",
+        database="face-recognition")
+        cursor = mydb.cursor(buffered=True)
+        if not mydb.is_connected:
+            tkinter.messagebox.showinfo("Error while connecting to the database!")
         else:
+            if login:
+                print("a")
+                query1 = "SELECT `password` FROM `login` WHERE username='"+name+"'"
+                cursor.execute(query1)
+                print("b")
+                for row in cursor: 
+                    if row[0]==passw:
+                        print("loginn successfull")
+                        break   
+                print("oneeeeeeeeeeeyyye")    
+                cursor.close()
+                mydb.close()
+              
+            else:
             # print("signup ho gya") 
-            query1 = "INSERT INTO `login` (`username`, `password`, `Code number`) VALUES ('"+name+"', '"+passw+"', '"+code+"')"
-            cursor.execute(query1)
-            cursor.execute("commit")   
-            cursor.close()
-            mydb.close()
+                query1 = "INSERT INTO `login` (`username`, `password`, `Code number`) VALUES ('"+name+"', '"+passw+"', '"+code+"')"
+                cursor.execute(query1)
+                cursor.execute("commit")   
+                cursor.close()
+                mydb.close()
         root1.destroy()
         print("oneeeeeeeeeeee")
         Dashboard_UI_Fun()
+    except error as E:
+        print(E)
