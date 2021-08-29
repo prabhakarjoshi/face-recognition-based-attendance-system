13532d5a4f12d267654c7a14b8fc38675b20f607
prabhakarjoshi <70520718+prabhakarjoshi@users.noreply.github.com>
2021-08-29 11:28:18 +0530

    final changes

--- Data_Processing/Take_Attendence.py
+++ Data_Processing/Take_Attendence.py
@@ -1,10 +1,12 @@
+from DB.Mark_DB import Mark_DB_Fun
 import cv2
 from DB.Get_Name_DB import Get_Name_DB_Fun
 def Take_Attendence_Fun():
     clf=cv2.face.LBPHFaceRecognizer_create()
-    clf.read("trainer/trainer.yml")
-    face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    # list=[]
+    
+    face_cascade=cv2.CascadeClassifier("Support/haarcascade_frontalface_default.xml")
+    clf.read("Support/trainer.yml")
+    a={}
     def draw_boundary(img,scalefactor,minneighbours):
         gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
         features=face_cascade.detectMultiScale(gray,scalefactor,minneighbours)
@@ -15,10 +17,10 @@ def Take_Attendence_Fun():
             confidence=int((100*(1-predict/300)))
 
             if confidence>70:
-                print(str(id))
                 a=Get_Name_DB_Fun(id)
-                print(a)
-
+                global myset
+                myset=set()
+                myset.add(id)
                 cv2.putText(img,a, (x-2,y+h-2),cv2.FONT_HERSHEY_DUPLEX,1,(0,0,255),1)
 
     def recog(img):
@@ -34,5 +36,7 @@ def Take_Attendence_Fun():
             cv2.imshow("welcome",img)
             if cv2.waitKey(1)==13:
                 break
+    # print(myset)  
+    Mark_DB_Fun(myset)      
     video_cap.release()
     cv2.destroyAllWindows() 
