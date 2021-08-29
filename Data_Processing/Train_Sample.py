3e9677de6a63cd759e2b7e829b80f6b28b9f1be2
prabhakarjoshi <70520718+prabhakarjoshi@users.noreply.github.com>
2021-08-29 11:29:44 +0530

    final changes

--- Data_Processing/Train_Sample.py
+++ Data_Processing/Train_Sample.py
@@ -1,5 +1,6 @@
 import os
 from tkinter import *
+import tkinter
 # import tkinter
 # import tkinter.messagebox
 import numpy as np
@@ -8,7 +9,7 @@ from PIL import Image
 
 def Train_Sample_Fun():
     recognizer = cv2.face.LBPHFaceRecognizer_create()
-    detector= cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    detector= cv2.CascadeClassifier("Support/haarcascade_frontalface_default.xml")
 
     def getImagesAndLabels(path):
         imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
@@ -28,26 +29,7 @@ def Train_Sample_Fun():
 
     faces,Ids = getImagesAndLabels('Data')
     recognizer.train(faces, np.array(Ids))
-    recognizer.save('trainer/trainer.yml')
-    print("Data Trained")
-    # dir=("Data")
-    # path=[os.path.join(dir,file) for file in os.listdir(dir)]
-    # faces=[]
-    # ids=[]
-
-    # for image in path:
-    #     img=Image.open(image).convert('L')
-    #     imagenp=np.array(img,'uint8')
-    #     id=int(os.path.split(image)[1].split('_')[0])
-    #     faces.append(imagenp)
-    #     ids.append(id)
-    #     cv2.imshow("training",imagenp)
-    #     cv2.waitKey(1)==13
-    # ids=np.array(ids) 
-    # print(ids)   
-
-    # clf=cv2.face.LBPHFaceRecognizer_create()
-    # clf.train(faces,ids)
-    # clf.write("classifier.yml")
-    # cv2.destroyAllWindows()
-    # tkinter.messagebox.showinfo("done","trained dataset")
+    recognizer.save('Support/trainer.yml')
+    # print("Data Trained")
+    tkinter.messagebox.showinfo("done","trained dataset")
+   
